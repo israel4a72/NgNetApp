d3dc5ea9e8027667933e9a2aa3ee7a1ea4512627
Israel <israelssilva16jrjunior@gmail.com>
2023-02-16 20:07:35 -0300

    Adding DB, Entity Framework and User Controller

---
+++ Backend/API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            user ??= new User();
+
+            return user;
+        }
+    }
+}
