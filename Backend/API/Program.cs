b3942b2496b43c0296a52a5f9225d01293b6df9a
Silva <israelssilva16jrjunior@gmail.com>
2023-02-18 13:27:29 -0300

    Created Client in Angular / + Bootstrap

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -8,8 +8,15 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => {
+    x.AllowAnyHeader();
+    x.AllowAnyMethod();
+    x.WithOrigins("https://localhost:4200");
+});
 
 app.MapControllers();
 
